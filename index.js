f0530867a2927af78ee5cb7ebcf87960c5e302fe
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-06 07:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon {
+  constructor (sides = []){
+    this.sides =sides;
+  }
+  get countSides (){
+    return  this.sides.length;
+  }
+  get perimeter  (){
+    const reducer = (accu, currVal) => accu + currVal;
+    return  this.sides.reduce(reducer , 0);
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid  (){
+    if(this.countSides === 3) {
+      if(this.sides[0] + this.sides[1] >= this.sides[2] &&
+         this.sides[0] + this.sides[2] >= this.sides[1] &&
+         this.sides[1] + this.sides[2] >= this.sides[0]){
+           return true ;
+         }
+
+    }
+    return false ;
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid  (){
+    let flag  ;
+    if(this.countSides === 4) {
+      for (let i = 1 ; i < this.countSides ; i++) {
+        flage = this.sides[0] === this.sides[i];
+        if(!flage) {
+          return false ;
+        }
+      }
+      return flage ;
+    }
+
+
+  }
+}
